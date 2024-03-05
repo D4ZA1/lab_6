cbaf90f3825fa38aede87e239e4d0a4b262a33ae
Pasupuleti Jaswanth <139963394+B4NK411@users.noreply.github.com>
2024-03-05 12:02:37 +0530

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,9 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from math import exp
 
 
-def activate(x):
+def step_activation(x):
     return x>0
+
+def sigmoid_activation(x):
+    return 1/(1+exp(-x))
+
+def bi_polar_activation(x):
+    if x>0:
+        return 1
+    elif x==0:
+        return 0
+    else:
+        return -1
+
+def reLU_activation(x):
+    if x>0:
+        return x
+    else:
+        return 0
 
 
 training_data = np.array([[0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 1]])
@@ -14,11 +32,11 @@ errors = []
 for epoch in range(epochs):
     error_sum = 0
     for x in training_data:
-        output = activate(W0*x[0] + W1*x[1] + W2*1)
+        output = step_activation(W0*x[0] + W1*x[1] + W2*1)
         error = x[2] - output
         W0 += alpha * error * x[0]
         W1 += alpha * error * x[1]
-        W2 += alpha * error * 1 
+        W2 += alpha * error * 1
         error_sum += error**2
     errors.append(error_sum)
     if error_sum<=0.002:
@@ -31,3 +49,4 @@ plt.xlabel('Epochs')
 plt.ylabel('Sum-Square-Error')
 plt.title('Error vs. Epochs')
 plt.show()
+
